b509d4c0f4a3205b71e9c0db32fc1952e8eb5ff1
cac03 <happy96538@gmail.com>
2017-01-24 12:30:29 +0300

    Add product hunt interface

---
+++ app/src/main/java/com/caco3/producthunt/producthunt/ProductHunt.java
@@ -0,0 +1,8 @@
+package com.caco3.producthunt.producthunt;
+
+
+import com.caco3.producthunt.producthunt.category.CategoriesService;
+
+public interface ProductHunt {
+  CategoriesService categories();
+}
