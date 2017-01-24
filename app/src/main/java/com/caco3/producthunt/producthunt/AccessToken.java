c3316b3543e8cd994cc3d4f6ecce901617ec98d4
cac03 <happy96538@gmail.com>
2017-01-24 13:04:58 +0300

    Add AccessToken model

---
+++ app/src/main/java/com/caco3/producthunt/producthunt/AccessToken.java
@@ -0,0 +1,14 @@
+package com.caco3.producthunt.producthunt;
+
+
+public class AccessToken {
+  private final String token;
+
+  public AccessToken(String token) {
+    this.token = token;
+  }
+
+  public String getToken() {
+    return token;
+  }
+}
