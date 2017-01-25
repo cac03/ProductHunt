60d64117c262b67cd126cd07f5824b65db4fff7a
cac03 <happy96538@gmail.com>
2017-01-25 21:22:11 +0300

    Add showPost method to PostView interface

--- app/src/main/java/com/caco3/producthunt/post/PostView.java
+++ app/src/main/java/com/caco3/producthunt/post/PostView.java
@@ -4,5 +4,6 @@ import com.caco3.producthunt.mvp.BaseView;
 import com.caco3.producthunt.producthunt.posts.ProductHuntPost;
 
 public interface PostView extends BaseView {
+  void showPost(ProductHuntPost post);
   void openPostProductSite(ProductHuntPost post);
 }
