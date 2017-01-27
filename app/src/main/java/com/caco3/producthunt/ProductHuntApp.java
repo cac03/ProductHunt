cdd008854f70cc24164f49139ec0c3b922a4996c
cac03 <happy96538@gmail.com>
2017-01-27 19:23:42 +0300

    Schedule CategoriesAndPostsUpdateService when app first run

--- app/src/main/java/com/caco3/producthunt/ProductHuntApp.java
+++ app/src/main/java/com/caco3/producthunt/ProductHuntApp.java
@@ -1,18 +1,30 @@
 package com.caco3.producthunt;
 
 import android.app.Application;
+import android.preference.PreferenceManager;
 
+import com.caco3.producthunt.autoupdate.CategoriesAndPostsAutoUpdateAlarmReceiver;
 import com.caco3.producthunt.dagger.DaggerComponentsHolder;
+import com.caco3.producthunt.util.Alarms;
+
+import java.util.concurrent.TimeUnit;
 
 import timber.log.Timber;
 
 public class ProductHuntApp extends Application {
+  private static final String FIRST_RUN_KEY = "first_run";
+  static final long CATEGORIES_AND_POSTS_AUTO_UPDATE_INTERVAL
+          = TimeUnit.HOURS.toMillis(1);
 
   @Override
   public void onCreate() {
     super.onCreate();
     initTimber();
     initDaggerApplicationComponent();
+    if (isFirstRun()) {
+      scheduleCategoriesAndPostsUpdateService();
+      setFirstRun(false);
+    }
   }
 
   private void initTimber() {
@@ -24,4 +36,22 @@ public class ProductHuntApp extends Application {
             .getInstance()
             .initApplicationComponent(this);
   }
+
+  private void scheduleCategoriesAndPostsUpdateService() {
+    Alarms.scheduleAlarm(this,
+            CategoriesAndPostsAutoUpdateAlarmReceiver.class,
+            CATEGORIES_AND_POSTS_AUTO_UPDATE_INTERVAL);
+  }
+
+  private boolean isFirstRun() {
+    return PreferenceManager.getDefaultSharedPreferences(this)
+            .getBoolean(FIRST_RUN_KEY, true);
+  }
+
+  private void setFirstRun(boolean firstRun) {
+    PreferenceManager.getDefaultSharedPreferences(this)
+            .edit().putBoolean(FIRST_RUN_KEY, firstRun)
+            .apply();
+  }
+
 }
